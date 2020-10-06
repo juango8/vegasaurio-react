b1f222ed8096aea58605dcb9c3bc67ef15b37e05
unknown <eder.cornejo.diaz@ucsp.edu.pe>
2020-10-05 22:55:10 -0500

    add selector

--- src/components/pages/products/ProductsHome.js
+++ src/components/pages/products/ProductsHome.js
@@ -39,6 +39,55 @@ export const ProductsHome = () => {
                         </ul>
                     </div>
                 </div>
+                <div className="col-lg-9 col-12">
+                    <div className="tags">
+                        <div class="select-box">
+                            <div class="select-box__current" tabindex="1">
+                                <div class="select-box__value">
+                                    <input class="select-box__input" type="radio" id="0" value="1" name="Ben" checked="checked" />
+                                    <p class="select-box__input-text">Cream</p>
+                                </div>
+                                <div class="select-box__value">
+                                    <input class="select-box__input" type="radio" id="1" value="2" name="Ben" checked="checked" />
+                                    <p class="select-box__input-text">Cheese</p>
+                                </div>
+                                <div class="select-box__value">
+                                    <input class="select-box__input" type="radio" id="2" value="3" name="Ben" checked="checked" />
+                                    <p class="select-box__input-text">Milk</p>
+                                </div>
+                                <div class="select-box__value">
+                                    <input class="select-box__input" type="radio" id="3" value="4" name="Ben" checked="checked" />
+                                    <p class="select-box__input-text">Honey</p>
+                                </div>
+                                <div class="select-box__value">
+                                    <input class="select-box__input" type="radio" id="4" value="5" name="Ben" checked="checked" />
+                                    <p class="select-box__input-text">Toast</p>
+                                </div><img class="select-box__icon" src="http://cdn.onlinewebfonts.com/svg/img_295694.svg" alt="Arrow Icon" aria-hidden="true" />
+                            </div>
+                            <ul class="select-box__list">
+                                <li>
+                                    <label class="select-box__option" for="0" aria-hidden="aria-hidden">Cream</label>
+                                </li>
+                                <li>
+                                    <label class="select-box__option" for="1" aria-hidden="aria-hidden">Cheese</label>
+                                </li>
+                                <li>
+                                    <label class="select-box__option" for="2" aria-hidden="aria-hidden">Milk</label>
+                                </li>
+                                <li>
+                                    <label class="select-box__option" for="3" aria-hidden="aria-hidden">Honey</label>
+                                </li>
+                                <li>
+                                    <label class="select-box__option" for="4" aria-hidden="aria-hidden">Toast</label>
+                                </li>
+                            </ul>
+                        </div>
+                        <input type="checkbox" id="cb1" /><label className="labeltag" for="cb1">glutten free</label>
+                        <input type="checkbox" id="cb1" /><label className="labeltag" for="cb1">organico</label>
+                        <input type="checkbox" id="cb1" /><label className="labeltag" for="cb1">ecologico</label>
+                        <input type="checkbox" id="cb1" /><label className="labeltag" for="cb1">apto para diabeticos</label>
+                    </div>
+                </div>
             </div>
         </div>
     );
